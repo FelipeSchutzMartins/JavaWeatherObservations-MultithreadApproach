5f5962d2b0ac7d977d4b77ca4512026f787dd869
Felipe <felipeschutz2025@hotmail.com>
2023-02-10 00:35:12 -0300

    :pencil: removed unecessary code

--- src/main/java/com/weatherObservation/controller/WeatherBalloonController.java
+++ src/main/java/com/weatherObservation/controller/WeatherBalloonController.java
@@ -1,17 +1,11 @@
 package com.weatherObservation.controller;
 
-import com.weatherObservation.dto.request.GenerateFilteredFileRequest;
-import com.weatherObservation.dto.response.GenerateFlightStatisticsResponse;
 import com.weatherObservation.service.GenerateDataFileService;
-import com.weatherObservation.service.GenerateFilteredFileService;
-import com.weatherObservation.service.GenerateFlightStatisticsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
@@ -22,25 +16,10 @@ import java.text.ParseException;
 @Controller
 public class WeatherBalloonController {
     private GenerateDataFileService generateDataFileService;
-    private GenerateFilteredFileService generateFilteredFileService;
-    private GenerateFlightStatisticsService generateFlightStatisticsService;
 
     @PostMapping("/multiThreadGenerateFile")
     public ResponseEntity<?> generateFile() throws IOException, ParseException, InterruptedException {
         generateDataFileService.generateFile();
         return ResponseEntity.ok(HttpStatus.OK);
     }
-
-    @PostMapping("/convertData")
-    public ResponseEntity<?> convertData(
-            @RequestBody GenerateFilteredFileRequest request) throws Exception {
-        generateFilteredFileService.generateFilteredFile(request);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
-
-    @GetMapping("/flightStatistics")
-    public ResponseEntity<GenerateFlightStatisticsResponse> flightStatistics() throws Exception {
-        return ResponseEntity.ok(generateFlightStatisticsService.generateFlightStatistics());
-    }
-
 }
